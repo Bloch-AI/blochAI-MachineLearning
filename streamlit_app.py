43e36924652f1d3c24c1c7a4750ab5134a65ac1d
Jamie Crossman-Smith <jamie@bloch.ai>
2024-08-05 16:33:48 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 # GitHub URL for the dataset
-url = 'https://github.com/Bloch-AI/blochAI-MachineLearning/wine.xlsx
+url = 'https://github.com/Bloch-AI/blochAI-MachineLearning/wine.xlsx'
 
 # Function to load data from GitHub
 @st.cache
