344e46f163cb2299346a8435bc48e8c68aabb6b7
Jamie Crossman-Smith <jamie@bloch.ai>
2024-08-05 16:43:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests
+import openpyxl
 from io import BytesIO
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
